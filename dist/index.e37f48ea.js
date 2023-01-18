c8c49ad0ac49faf677bec498497f71c24105fc87
Vinicius <vinicius_henrique96@hotmail.com>
2023-01-18 11:47:23 -0300

    :sparkles: feat: Implementing Error and Success Messages:

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -554,7 +554,7 @@ const controlRecipes = async function(e) {
         // 2) Rendering recipe
         (0, _recipeViewJsDefault.default).render(_modelJs.state.recipe);
     } catch (err) {
-        alert(err);
+        (0, _recipeViewJsDefault.default).renderError();
     }
 };
 const init = function() {
@@ -617,7 +617,7 @@ const loadRecipe = async function(id) {
             ingredients: recipe.ingredients
         };
     } catch (err) {
-        console.error(`${err} 💥💥💥💥💥`);
+        throw err;
     }
 };
 
@@ -1255,11 +1255,13 @@ var _fractional = require("fractional");
 class RecipeView {
     #parentElement = document.querySelector(".recipe");
     #data;
+    #errorMessage = "We could not find that recipe. Please try another one!";
+    #message = "";
     // API publica
     render(data) {
         this.#data = data;
         const markup = this.#generateMarkup();
-        this.#clear;
+        this.#clear();
         this.#parentElement.insertAdjacentHTML("afterbegin", markup);
     }
     #clear() {
@@ -1274,7 +1276,35 @@ class RecipeView {
           </svg>
         </div>
     `;
-        this.#parentElement.innerHTML = "";
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+    }
+    renderError(message = this.#errorMessage) {
+        const markup = `
+        <div class="error">
+          <div>
+            <svg>
+              <use href="${(0, _iconsSvgDefault.default)}#icon-alert-triangle"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div> 
+    `;
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+    }
+    renderMessage(message = this.#message) {
+        const markup = `
+        <div class="message">
+          <div>
+            <svg>
+              <use href="${(0, _iconsSvgDefault.default)}#icon-smile"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div> 
+    `;
+        this.#clear();
         this.#parentElement.insertAdjacentHTML("afterbegin", markup);
     }
     // Estamos realizando aqui o Publisher-Subscriber Pattern => Este metodo, é que o editor basicamente precisa ter acesso ao assinante, e nessa caso, é a função de manipulador. É uma API publica para que podemos chamar no controller.js
